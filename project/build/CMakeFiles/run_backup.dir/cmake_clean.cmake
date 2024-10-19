578124eebd1c3f1406b388c19f35b1e2762b1b90
meissa20 <157523405+meissa20@users.noreply.github.com>
2024-10-20 00:05:10 +0300

    Delete project directory

--- project/build/CMakeFiles/run_backup.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_backup"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_backup.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
